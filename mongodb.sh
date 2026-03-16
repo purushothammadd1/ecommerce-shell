44a26a6f2a911cdb7164dd7e192f8034e4f0637c
Purushotham Maddi <purushothammaddi890@gmail.com>
2026-03-16 23:16:50 +0530

    mongodb shell script added

---
+++ mongodb.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 #you can give other than 0
+else
+    echo -e "$G You are root user $N"
+fi #fi means reverse of if, indicating condition end
+
+cp mongo.repo /etc/yum.repos.d/ &>> LOGFILE
+
+VALIDATE $? "Copied MongoDB Repo"
