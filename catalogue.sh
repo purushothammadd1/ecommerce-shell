858429c25c211de31c373400673fa6d124350209
Purushotham Maddi <purushothammaddi890@gmail.com>
2026-03-17 21:58:46 +0530

    catalogue script added

---
+++ catalogue.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+MONGDB_HOST=mongodb.purushothamai.online
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 #you can give other than 0
+else
+    echo -e "$G You are root user $N"
+fi #fi means reverse of if, indicating condition end
+
+dnf module disable nodejs -y &>> $LOGFILE
+VALIDATE $? "Disabling current NodeJS"
+
+dnf module enable nodejs:18 -y &>> $LOGFILE
+VALIDATE $? "Enabling NodeJS:18"
+
+dnf install nodejs -y &>> $LOGFILE
+VALIDATE $? "Installing NodeJS:18"
+
+useradd roboshop &>> $LOGFILE
+VALIDATE $? "creating roboshop var"
+
+mkdir /app &>> $LOGFILE
+VALIDATE $? "creating app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip  &>> $LOGFILE
+VALIDATE $? "Downloading catalogue application"
+
+cd /app
+
+unzip -o /tmp/catalogue.zip &>> $LOGFILE
+VALIDATE $? "unzipping catalogue"
+
+npm install &>> $LOGFILE
+VALIDATE $? "Installing dependencies"
+
+# use absolute, because catalogue.service exists there
+cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>> $LOGFILE
+VALIDATE $? "Copying catalogue service file"
+
+systemctl daemon-reload &>> LOGFILE
+VALIDATE $? "Catalogue daemon reload"
+
+systemctl enable catalogue &>> $LOGFILE
+VALIDATE $? "Enable Catalogue"
+
+systemctl start catalogue &>> $LOGFILE
+VALIDATE $? "starting catalogue"
+
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "copying mongodb repo"
+
+yum install mongodb-org-shell -y &>> $LOGFILE
+VALIDATE $? "Installing MongoDB client"
+
+mongo --host $MONGDB_HOST </app/schema/catalogue.js &>> $LOGFILE
+VALIDATE $? "Loading catalogue data into MongoDB"
