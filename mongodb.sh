bbfa58c8bf1975c98ba5e5056eb3a77e77dce187
Purushotham Maddi <purushothammaddi890@gmail.com>
2026-04-17 22:31:34 +0530

    user script added

--- mongodb.sh
+++ mongodb.sh
@@ -34,7 +34,7 @@ cp mongo.repo /etc/yum.repos.d/ &>> LOGFILE
 
 VALIDATE $? "Copied MongoDB Repo"
 
-yum install mongodb-org -y &>> $LOGFILE
+dnf install mongodb-org -y &>> $LOGFILE
 
 VALIDATE $? "Installing MongoDB"
 
