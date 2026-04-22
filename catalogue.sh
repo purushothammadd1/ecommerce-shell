c67bca4dda183ec8163296db9b451a209f8a4395
Purushotham Maddi <purushothammaddi890@gmail.com>
2026-04-22 15:34:56 +0530

    catalogue script changes

--- catalogue.sh
+++ catalogue.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 ID=$(id -u)
-
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-MONGDB_HOST=mongodb.purushothamai.online
+MONGDB_HOST=mongodb.daws76s.online
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
-echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -26,61 +25,75 @@ VALIDATE(){
 if [ $ID -ne 0 ]
 then
     echo -e "$R ERROR:: Please run this script with root access $N"
-    exit 1 #you can give other than 0
+    exit 1 # you can give other than 0
 else
-    echo -e "$G You are root user $N"
-fi #fi means reverse of if, indicating condition end
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
 
 dnf module disable nodejs -y &>> $LOGFILE
+
 VALIDATE $? "Disabling current NodeJS"
 
-dnf module enable nodejs:18 -y &>> $LOGFILE
+dnf module enable nodejs:18 -y  &>> $LOGFILE
+
 VALIDATE $? "Enabling NodeJS:18"
 
-dnf install nodejs -y &>> $LOGFILE
+dnf install nodejs -y  &>> $LOGFILE
+
 VALIDATE $? "Installing NodeJS:18"
 
-id ecommerce
+id roboshop #if roboshop user does not exist, then it is failure
 if [ $? -ne 0 ]
 then
-    useradd ecommerce
-    VALIDATE $? "ecommerce user creation"
+    useradd roboshop
+    VALIDATE $? "roboshop user creation"
 else
-    echo -e "E-commerce user already exist $Y Skipping $N"
+    echo -e "roboshop user already exist $Y SKIPPING $N"
 fi
 
-mkdir -p /app &>> $LOGFILE
+mkdir -p /app
+
 VALIDATE $? "creating app directory"
 
 curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip  &>> $LOGFILE
+
 VALIDATE $? "Downloading catalogue application"
 
-cd /app
+cd /app 
 
-unzip -o /tmp/catalogue.zip &>> $LOGFILE
+unzip -o /tmp/catalogue.zip  &>> $LOGFILE
+
 VALIDATE $? "unzipping catalogue"
 
-npm install &>> $LOGFILE
+npm install  &>> $LOGFILE
+
 VALIDATE $? "Installing dependencies"
 
 # use absolute, because catalogue.service exists there
 cp /home/centos/ecommerce-shell/catalogue.service /etc/systemd/system/catalogue.service &>> $LOGFILE
+
 VALIDATE $? "Copying catalogue service file"
 
 systemctl daemon-reload &>> $LOGFILE
-VALIDATE $? "Catalogue daemon reload"
+
+VALIDATE $? "catalogue daemon reload"
 
 systemctl enable catalogue &>> $LOGFILE
-VALIDATE $? "Enable Catalogue"
+
+VALIDATE $? "Enable catalogue"
 
 systemctl start catalogue &>> $LOGFILE
-VALIDATE $? "starting catalogue"
+
+VALIDATE $? "Starting catalogue"
 
 cp /home/centos/ecommerce-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+
 VALIDATE $? "copying mongodb repo"
 
-yum install mongodb-org-shell -y &>> $LOGFILE
+dnf install mongodb-org-shell -y &>> $LOGFILE
+
 VALIDATE $? "Installing MongoDB client"
 
 mongo --host $MONGDB_HOST </app/schema/catalogue.js &>> $LOGFILE
-VALIDATE $? "Loading catalogue data into MongoDB"
+
+VALIDATE $? "Loading catalouge data into MongoDB"
