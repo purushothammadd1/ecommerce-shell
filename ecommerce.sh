c31cb92d866e3694f22e40ef954cc5efe571dd34
Purushotham Maddi <purushothammaddi890@gmail.com>
2026-05-05 22:25:02 +0530

    ecommerce file added

---
+++ ecommerce.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+AMI = "ami-0b4f379183e5706b9"
+SG_ID = "sg-05ce96a47ef68eb4c"
+INSTANCES = ("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+
+for i in "${INSTANCES[@]}"
+do
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    then
+        INSTANCE_TYPE="t3.small"
+    else
+        INSTANCE_TYPE="t3.micro"
+    fi
+
+aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-05ce96a47ef68eb4c
+done
